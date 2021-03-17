49c819b437f28ac81822d282fdad0826ceb8765d
sebaslh12 <sebaslh12@gmail.com>
2021-03-17 09:03:08 -0500

    feat: add file iteration

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { FileData } from './constants';
+import { getFiles } from './request';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        File Test
-      </header>
-    </div>
-  );
+	const [files, setFiles] = useState<FileData[]>([]);
+
+	useEffect(() => {
+		(async () => {
+			const result = await getFiles();
+			setFiles(result);
+		})()
+	}, [])
+	return (
+		<div className="App">
+			<h1>Files</h1>
+			<div className="files">
+				{files.map((file: FileData) =>
+					<div key={file.blobName}>
+						<a href={`https://qorus-test.azurewebsites.net/QorusFile/${file.blobName}`} target="_blank" rel="noreferrer">{file.fileName}</a>
+						<span>{file.size}</span>
+						<span>{file.category}</span>
+						<span>{file.lastReviewed}</span>
+					</div>
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default App;
